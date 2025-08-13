9ddd9dfac02c03c314d723c8624fd9e66800d512
hotwhiskey <joshuawaynebird@gmail.com>
2025-08-13 10:38:05 -0400

    audio added - start game with enter added

--- main.gd
+++ main.gd
@@ -8,6 +8,8 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
@@ -15,6 +17,8 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_mob_timer_timeout() -> void:
 	# Create a new instance of the Mob scene.
